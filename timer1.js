1f85fdfc6c9bdf2650f99ab8fb247ea0b0b973b5
danispinxo <genericpronoun@gmail.com>
2022-08-02 11:34:40 +0000

    Simple timer that beeps at user-given seconds

--- timer1.js
+++ timer1.js
@@ -1,9 +1,26 @@
 let userInput = process.argv.slice(2);
 
 const timerFunction = (userInput) => {
-  userInput.sort((a, b) => a - b);
-  console.log(userInput);
+  let validInput = [];
 
+  for (let elem of userInput) {
+    if (elem > 0) {
+      validInput.push(elem);
+    }
+  }
+
+  for (let i = 0; i < validInput.length; i++) {
+
+    setTimeout( () => {
+      process.stdout.write('\x07');
+    }, 1000 * (validInput[i]));
+  };
+  
+    setTimeout( () => {
+      console.log('\n');
+    }, 1000 * (validInput[validInput.length + 1]));
+
+  return;
   
 };
 
